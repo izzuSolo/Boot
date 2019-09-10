55f1a9d0210b97c001b0d4aa5471a737bbaee253
izzuSoloQ <skat-senin@yandex.ru>
2019-09-10 19:38:17 +0300

    first App

---
+++ src/main/java/io/SpringBoot/learn/CoursApiApp.java
@@ -0,0 +1,11 @@
+package io.SpringBoot.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursApiApp {
+    public static void main(String args[]){
+        SpringApplication.run(CoursApiApp.class, args);
+    }
+}
