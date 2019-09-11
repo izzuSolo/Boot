3b9e563e8fece46103d2e99badceda467a4a1ea3
izzuSoloQ <skat-senin@yandex.ru>
2019-09-11 18:08:18 +0300

    Topic

---
+++ src/main/java/io/SpringBoot/learn/topic/TopicController.java
@@ -0,0 +1,19 @@
+package io.SpringBoot.learn.topic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return Arrays.asList(
+                new Topic("Spring", "Framework", "Description"),
+                new Topic("Java", "1.8", "Core")
+        );
+    }
+}
