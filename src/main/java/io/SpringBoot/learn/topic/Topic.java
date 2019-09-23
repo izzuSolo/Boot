5a8d4abdd34a831ea79970d22c117e152a3e5c6b
izzuSolo <skat-senin@yandex.ru>
2019-09-23 21:53:21 +0300

    DATA

--- src/main/java/io/SpringBoot/learn/topic/Topic.java
+++ src/main/java/io/SpringBoot/learn/topic/Topic.java
@@ -1,7 +1,12 @@
 package io.SpringBoot.learn.topic;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
 
+    @Id
     private String id;
     private String name;
     private String description;
---
+++ src/main/java/io/SpringBoot/learn/topic/TopicRepository.java
@@ -0,0 +1,4 @@
+package io.SpringBoot.learn.topic;
+
+public class TopicRepository {
+}
